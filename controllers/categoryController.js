57669980eb01d06c82180b73878eae1cc3a5889b
DESKTOP-EFE9CH8\X1 <thaongan.0912@gmail.com>
2024-06-17 23:46:43 +0700

    Add Specific category route

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -8,6 +8,21 @@ exports.getCategories = async (req, res, next) => {
       next(err);
     }
   };
+
+//get specific category
+exports.getCategoryId = async (req, res, next) => {
+  try {
+    const {id} = req.params;
+    const category = await Category.findById(id);
+
+    if (!category) {
+      return res.status(404).json({message: 'Category not found'});
+    }
+    res.status(200).json(category)
+  } catch (err) {
+    next(err);
+  }
+}
   
   exports.addCategory = async (req, res, next) => {
     try {
