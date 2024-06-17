d3d5ccb6d6920fe158acd62dc75263a3242a4b73
DESKTOP-EFE9CH8\X1 <thaongan.0912@gmail.com>
2024-06-17 23:47:43 +0700

    API Category specific

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getCategories, addCategory } = require('../controllers/categoryController');
+const { getCategories,getCategoryId, addCategory } = require('../controllers/categoryController');
 
 const router = express.Router();
 
 router.get('/', getCategories);
+router.get('/:id', getCategoryId);
 router.post('/', addCategory);
 
 module.exports = router;
